98202839bcbe99f92f01d70ade93f9ad1316e55c
Ruby <55691511+okpc579@users.noreply.github.com>
2022-05-23 14:36:27 +0900

    first commit

---
+++ src/main/java/com/sideproject/spseureka/SpsEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.sideproject.spseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpsEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsEurekaApplication.class, args);
+	}
+
+}
